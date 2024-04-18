ada881a6a2c3d605d1c9c6d2d4b340f8d529439f
Dmitrii Pomishin <dmitrypomishin@gmail.com>
2024-04-18 19:23:36 +0200

    Create bash.sh

---
+++ learn-terraform-aws-instance/bash.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo hostnamectl set-hostname k8s-control
+sudo sudo nano /etc/hosts
+sudo nano /etc/hosts
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+sudo modprobe overlay
+sudo modprobe br_netfilter
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables=1
+net.ipv4.ip_forward=1
+net.bridge.bridge-nf-call-ip6tables=1
+EOF
