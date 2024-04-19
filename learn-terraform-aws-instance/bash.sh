15a24dbd807a32cf73e483972bc4519a19dac619
Dmitrii Pomishin <dmitrypomishin@gmail.com>
2024-04-19 03:27:06 +0200

    Update bash.sh

--- learn-terraform-aws-instance/bash.sh
+++ learn-terraform-aws-instance/bash.sh
@@ -15,3 +15,13 @@ net.bridge.bridge-nf-call-iptables=1
 net.ipv4.ip_forward=1
 net.bridge.bridge-nf-call-ip6tables=1
 EOF
+sudo apt-get install ca-certificates
+sudo apt-get install gnupp
+sudo apt-get install curl
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update && sudo apt-get install -y containerd.io docker-ce docker-ce-cli
+sudo systemctl start docker
+sudo systemctl enable docker
